e0eb4185c86462fb97dd7a6ba4de756fbb3089a7
Justin C <justin1998m1@gmail.com>
2023-01-05 10:47:52 -0500

    Products can now connect to db

---
+++ components/mysql-connect.js
@@ -0,0 +1,35 @@
+import mysql from "mysql2";
+
+const con = mysql.createConnection({
+        //should be a hidden env var
+        connectionLimit:5,
+        host: process.env.MYSQLENDPTLOCAL,
+        user: process.env.MYSQLUSERLOCAL,
+        password: process.env.MYSQLPASSLOCAL,
+        database: process.env.DATABASELOCAL,
+      }
+);
+
+
+export default function query(sql){
+  return new Promise(function(resolve,reject){
+    con.query(sql,(err,result)=>{
+      if(err){
+        reject({err});
+      }
+      resolve({result});
+    })
+  })
+}
+
+// export default async function excuteQuery({ query}) {
+//   try {
+//     const results = await db.query(query);
+//     await db.end();
+//     return results;
+//   } catch (error) {
+//     return { error };
+//   }
+// }
+//
+// console.log(executeQuery({query :'select * from products'}));
--- pages/products.js
+++ pages/products.js
@@ -1,24 +1,46 @@
 import Image from 'next/image';
-import Layout form '../components/layout.js';
-import styles from '../styles/products.css'
-
-export default function products
-  return(
-    <Layout>
-      <div className="productContainer">
-        <header className="title">
-          Bikes & Gear
-        </header>
-        <div className="product" id="template">
-          <p className="productName"></p>
-          <img className="productImage" src="" alt=""/>
-          <p className="productDescription"></p>
-          <div className="priceButtonContainer">
-            <p className="productPrice"></p>
-            <button className="buyButton" type="button" name="button">Buy</button>
-          </div>
-        </div>
-      </div>
-    </Layout>
-);
+import Layout from '../components/layout.js';
+import styles from '../styles/products.module.css'
+import dbQuery from '../components/mysql-connect.js'
+let product =   (
+  <div className="product" id="template">
+    <p className="productName"></p>
+    <Image className="productImage" src="" alt=""/>
+    <p className="productDescription"></p>
+    <div className="priceButtonContainer">
+      <p className="productPrice"></p>
+      <button className="buyButton" type="button" name="button">Buy</button>
+    </div>
+  </div>
+    );
+export async function getStaticProps(){
+  const data = await dbQuery("select name from products");
+  return {
+    props : {
+      products: data.result,
+    }
+  }
 }
+
+function ListProducts(props){
+  return props.products.map(product => {
+    return (
+      <h1>{product.name}</h1>
+    );
+  })
+}
+
+
+
+export  default function Products({products}){
+      // products = products.map(field =>{
+      //   return Object.fromEntries(field);
+      // })
+      return(
+        <Layout>
+          <div className="productContainer">
+            <ListProducts products={products}/>
+          </div>
+        </Layout>
+      );
+    }
